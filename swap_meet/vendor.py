a6b439d0e095be1626d1130610be52f9c1947e66
Tatiana Didenko <tatiana.a.didenko@gmail.com>
2025-09-30 12:23:32 -0700

    Sign coworking agreement and complete Wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,30 @@
 class Vendor:
-    pass
+    """
+    Represents a swap-meet participant with an inventory of items.
+    """
+
+    def __init__(self, inventory=None):
+        """
+        Initialize a Vendor.
+
+        :param inventory: optional list of items; defaults to an empty list.
+        """
+        self.inventory = [] if inventory is None else list(inventory)
+
+    def add(self, item):
+        """
+        Add an item to the inventory and return the same item.
+        """
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        """
+        Remove an item from the inventory.
+
+        :return: the removed item, or False if the item is not present.
+        """
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        return False
